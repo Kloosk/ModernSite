eb6cdbb54849c29972c4952c06f655ab2d4623cc
Lukasz <lukasz.gras.97@gmail.com>
2020-03-05 15:38:20 +0100

    Added menu

--- src/Nav/Hamburger/Hamburger.js
+++ src/Nav/Hamburger/Hamburger.js
@@ -1,11 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
-import List from "./List/List";
-import Menu from "./Menu/Menu";
+import img from "../../img/menu/mount.jpg";
 
 const Container = styled.div`
   width: 25vw;
   padding: 10px;
+  cursor: pointer;
+`;
+const HamMenu = styled.div`
+  width: 35px;
+  height: 20px;
   cursor: pointer;
 `;
 const Line = styled.div`
@@ -35,12 +39,124 @@ const Line = styled.div`
   border-radius: inherit;
   }
 `;
+const Containe = styled.div`
+  transform: ${props => props.open ? 'translateX(0)' : 'translateX(-91vw)'}; 
+  transition: transform 0.5s ease-in;
+  z-index: 3;
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 90vw;
+  height: 100vh;
+  background-color: #fff;
+  color: #000;
+  font-size: 1.4rem;
+  background-image: url(${img});
+  background-position: center;
+  background-size: cover;
+`;
+const Exit = styled.div`
+  z-index: 3;
+  position: absolute;
+  top: 2px;
+  right: 15px;
+  font-size: 4rem;
+  font-weight: bold;
+  color: #000;
+  cursor: pointer;
+`;
+const Ul = styled.ul`
+z-index: 3;
+color: #000;
+list-style: none;
+`;
+const Li = styled.li`
+z-index: 4;
+opacity: 0.8;
+padding: 13px 13px;
+text-transform: uppercase;
+font-weight: bold;
+font-size: 1.2rem;
+background-color: #fff;
+border-bottom: 1px solid #fff;
+cursor: pointer;
+&:hover{
+opacity: 1;
+border-bottom: 1px solid blueviolet;
+}
+`;
+const Hr = styled.hr`
+  background: linear-gradient(blueviolet , blue);
+  height: 4px;
+`;
+const Title = styled.li`
+  cursor: auto;
+  padding: 20px 15px;
+  opacity: 1;
+  background-color: #fff;
+  font-weight: bold;
+`;
+const Lang = styled.li`
+  cursor: auto;
+  padding: 20px 15px;
+  opacity: 1;
+  background-color: #fff;
+  z-index: 5;
+  &:hover{
+  font-weight: bold;
+  }
+`;
+const LangShow = styled.ul`
+  margin-top: 10px;
+  display: none;
+  transform: translateY();
+ ${Lang}:hover & {
+     display: block;
+  }
+`;
 const Hamburger = () => {
+    const [open,setOpen] = useState(false);
+    const openMenu = () => {
+      setOpen(!open);
+    };
     return(
         <Container>
-            <Line></Line>
-            <List/>
-            <Menu/>
+            <HamMenu onClick={openMenu}>
+                <Line></Line>
+            </HamMenu>
+            <Containe open={open}>
+                <Exit onClick={openMenu}>&times;</Exit>
+                <Ul>
+                    <Title>MENU</Title>
+                    <Hr/>
+                    <Li>home</Li>
+                    <Li>shop</Li>
+                    <Li>products</Li>
+                    <Li>pages</Li>
+                    <Li>contact us</Li>
+                </Ul>
+                <Ul>
+                    <Title>ACCOUNT</Title>
+                    <Hr/>
+                    <Li>Default welcome msg!</Li>
+                    <Li>compare products</Li>
+                    <Li>sign in</Li>
+                    <Li>create an account</Li>
+                </Ul>
+                <Ul>
+                    <Title>SETTINGS</Title>
+                    <Hr/>
+                    <Ul>
+                        <Lang>LANGUAGE
+                            <LangShow>
+                                <Li>PL</Li>
+                                <Li>EN</Li>
+                            </LangShow>
+                        </Lang>
+
+                    </Ul>
+                </Ul>
+            </Containe>
         </Container>
     )
 };
--- src/Nav/Hamburger/Menu/Menu.js
+++ src/Nav/Hamburger/Menu/Menu.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, {useState} from "react";
 import styled from "styled-components";
 import img from '../../../img/menu/mount.jpg'
 const Container = styled.div`
+  transform: ${props => props.open ? 'translateX(0)' : 'translateX(-91vw)'}; 
   z-index: 3;
   position: fixed;
   top: 0;
@@ -14,21 +15,11 @@ const Container = styled.div`
   background-image: url(${img});
   background-position: center;
   background-size: cover;
-  &::after{
-    content: '';
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    background-color: #fff;
-    opacity: 0.6;
-  }
 `;
 const Exit = styled.div`
   z-index: 3;
   position: absolute;
-  top: 5px;
+  top: 2px;
   right: 15px;
   font-size: 4rem;
   font-weight: bold;
@@ -36,42 +27,92 @@ const Exit = styled.div`
   cursor: pointer;
 `;
 const Ul = styled.ul`
-z-index: 4;
-background-color: #fff;
+z-index: 3;
 color: #000;
+list-style: none;
 `;
 const Li = styled.li`
 z-index: 4;
+opacity: 0.8;
+padding: 13px 13px;
+text-transform: uppercase;
+font-weight: bold;
+font-size: 1.2rem;
+background-color: #fff;
+border-bottom: 1px solid #fff;
+cursor: pointer;
+&:hover{
+opacity: 1;
+border-bottom: 1px solid blueviolet;
+}
 `;
-const Menu = () => {
+const Hr = styled.hr`
+  background: linear-gradient(blueviolet , blue);
+  height: 4px;
+`;
+const Title = styled.li`
+  cursor: auto;
+  padding: 20px 15px;
+  opacity: 1;
+  background-color: #fff;
+  font-weight: bold;
+`;
+const Lang = styled.li`
+  cursor: auto;
+  padding: 20px 15px;
+  opacity: 1;
+  background-color: #fff;
+  z-index: 5;
+  &:hover{
+  font-weight: bold;
+  }
+`;
+const LangShow = styled.ul`
+  margin-top: 10px;
+  display: none;
+  transform: translateY();
+ ${Lang}:hover & {
+     display: block;
+  }
+`;
+const Menu = props => {
+    const [open,setOpen] = useState(false);
+    const closeMenu = () => {
+      setOpen(false);
+    };
     return(
-        <Container>
-            <Exit>&times;</Exit>
+        <Container open={open}>
+            <Exit onClick={closeMenu}>&times;</Exit>
             <Ul>
-                MENU
-                <hr/>
+                <Title>MENU</Title>
+                <Hr/>
                 <Li>home</Li>
-                <li>shop</li>
-                <li>products</li>
-                <li>pages</li>
-                <li>contact us</li>
+                <Li>shop</Li>
+                <Li>products</Li>
+                <Li>pages</Li>
+                <Li>contact us</Li>
             </Ul>
-            <ul>
-                ACCOUNT
-                <hr/>
-                <li>Default welcome msg!</li>
-                <li>compare products</li>
-                <li>sign in</li>
-                <li>create an account</li>
-            </ul>
-            <ul>
-                SETTINGS
-                <hr/>
-                <uL>LANG
-                    <li>PL</li>
-                    <li>EN</li>
-                </uL>
-            </ul>
+            <Ul>
+                <Title>ACCOUNT</Title>
+                <Hr/>
+                <Li>Default welcome msg!</Li>
+                <Li>compare products</Li>
+                <Li>sign in</Li>
+                <Li>create an account</Li>
+            </Ul>
+            <Ul>
+                <Title>SETTINGS</Title>
+                <Hr/>
+                <Ul>
+                    <Lang>LANGUAGE
+                        <LangShow>
+                            <Li>PL</Li>
+                            <Li>EN</Li>
+                        </LangShow>
+                    </Lang>
+
+                </Ul>
+            </Ul>
         </Container>
     )
 };
